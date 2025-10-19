f7f22977e48025e2196b9d8f63abea169bc3a0c6
Poonam Anand <anandpoonam85@gmail.com>
2025-10-19 09:35:12 -0700

    adding vercel to allowed cors

--- src/main/java/com/infinityisland/config/CorsFilter.java
+++ src/main/java/com/infinityisland/config/CorsFilter.java
@@ -25,10 +25,10 @@ public class CorsFilter implements Filter {
             System.getenv().getOrDefault("APP_CORS_ALLOWED_ORIGINS",
                     "http://localhost:5173," +
                             "http://127.0.0.1:5173," +
-                            "https://infinity-island-frontend-poonam-anands-projects.vercel.app/," +
-                            "infinity-island-frontend.vercel.app," +
-                            "infinity-island-frontend-git-main-poonam-anands-projects.vercel.app," +
-                            "infinity-island-frontend-4ypijt4j1-poonam-anands-projects.vercel.app")
+                            "https://infinity-island-frontend-poonam-anands-projects.vercel.app," +
+                            "https://infinity-island-frontend.vercel.app," +
+                            "https://infinity-island-frontend-git-main-poonam-anands-projects.vercel.app," +
+                            "https://infinity-island-frontend-4ypijt4j1-poonam-anands-projects.vercel.app")
     );
 
     @PostConstruct
